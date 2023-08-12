8b73ece34679ba912687a3e2ed55dcc49a421ac9
saalej <marian.maha.19@gmail.com>
2023-08-12 11:41:14 -0700

    Corrección de audio menu, tres gatos

--- Assets/Scripts/Menuinicial.cs
+++ Assets/Scripts/Menuinicial.cs
@@ -7,7 +7,7 @@ public class Menuinicial : MonoBehaviour
 {
     public void jugar()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("SampleScene");
     }
 
     public void salir()
