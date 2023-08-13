2ea581f281f935ab53d243c97ba1ee1d53ddbd03
saalej <marian.maha.19@gmail.com>
2023-08-13 01:02:08 -0700

    Al fin, movimiento

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
-    /*
+
     private Rigidbody2D rb2d;
 
     [Header("Movimiento")]
 
     private float _moveHorizontal;
-    [SerializeField] private float _speed;
+    [SerializeField] private float _speed = 500f;
     [Range(0, 0.3f)][SerializeField] private float _smoother;
 
     Vector3 _moveVelocity = Vector3.zero;
@@ -18,7 +18,7 @@ public class Move : MonoBehaviour
     private bool _isRight = false;
 
     [Header("Jump")]
-    [SerializeField] private float _jumpForce;
+    [SerializeField] private float _jumpForce = 500f;
     [SerializeField] private LayerMask _floor;
     [SerializeField] private Transform _floorController;
     [SerializeField] private Vector3 _boxDimensions;
@@ -45,10 +45,10 @@ public class Move : MonoBehaviour
 
     private void FixedUpdate()
     {
+        
         _isFloor = Physics2D.OverlapBox(_floorController.position, _boxDimensions, 0f, _floor);
-
         Motion(_moveHorizontal * Time.fixedDeltaTime, _jump);
-
+        
         _jump = false;
     }
 
@@ -57,12 +57,12 @@ public class Move : MonoBehaviour
         Vector3 _speedObject = new Vector2(mover, rb2d.velocity.y);
         rb2d.velocity = Vector3.SmoothDamp(rb2d.velocity, _speedObject, ref _moveVelocity, _smoother);
 
-        if((mover > 0 && !_isRight) || (mover < 0 && _isRight))
+        if ((mover > 0 && !_isRight) || (mover < 0 && _isRight))
         {
             Spin();
-        } 
+        }
 
-        if(_isFloor && saltar)
+        if (_isFloor && saltar)
         {
             _isFloor = false;
             rb2d.AddForce(new Vector2(0f, _jumpForce));
@@ -76,40 +76,10 @@ public class Move : MonoBehaviour
         _scale.x *= -1;
         transform.localScale = _scale;
     }
-    /*
+
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireCube(_floorController.position, _boxDimensions);
-    }
-    */
-
-    [SerializeField] private CharacterController2D _controller;
-
-    float _horizontalMove = 0f;
-
-    private bool _jump = false;
-
-    [SerializeField] float _runSpeed = 100f;
-
-    private void Start()
-    {
-        
-    }
-
-    private void Update()
-    {
-        _horizontalMove = Input.GetAxisRaw("Horizontal") * _runSpeed ;
-
-        if (Input.GetButtonDown("Jump"))
-        {
-            _jump = true;
-        }
-    }
-
-    private void FixedUpdate() 
-    {
-        _controller.Motion(_horizontalMove * Time.fixedDeltaTime, false, _jump);
-        _jump = false;
     }
 }
